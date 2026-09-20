8a603eb53ca739e0d4f32211b0a9f0422afd56b9
sea <hai.hachi.154@gmail.com>
2026-09-21 04:59:14 +0700

    Default mission stack to wall time

--- src/scrobot_mission/launch/sweep_mission.launch.py
+++ src/scrobot_mission/launch/sweep_mission.launch.py
@@ -75,7 +75,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='true',
+            default_value='false',
             choices=['true', 'false'],
         ),
         global_localization,
