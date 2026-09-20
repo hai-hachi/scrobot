bbd028c868aed5c1aa97dd2b2f2918a2e405a091
sea <hai.hachi.154@gmail.com>
2026-09-21 04:59:01 +0700

    Default shared control stack to wall time

--- src/scrobot_control/launch/control_stack.launch.py
+++ src/scrobot_control/launch/control_stack.launch.py
@@ -95,7 +95,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='true',
+            default_value='false',
             choices=['true', 'false'],
         ),
         joint_state_broadcaster_spawner,
